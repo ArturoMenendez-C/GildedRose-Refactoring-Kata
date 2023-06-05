4c8f66bf4f6f9f47b9d5ebd51fb2d750aa1dc739
Rebeca Gonzalez <bequipedia@gmail.com>
2023-06-05 14:14:23 +0200

    fix failing test and add golden master test approved

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -19,5 +19,12 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals(2, app.items[0].quality);
     }
+    @Test
+    void backstage_quality_should_increase_by_three_when_sellIn_lower_than_eleven_and_lower_than_six() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(3, app.items[0].quality);
+    }
 
 }
